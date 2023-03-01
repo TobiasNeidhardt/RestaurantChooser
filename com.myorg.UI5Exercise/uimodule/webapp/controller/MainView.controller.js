0d9d1430969b1acfaade1cdc05c710e240db52e0
Neidhardt <tobias.neidhardt@accenture.com>
2023-03-01 16:50:27 +0100

    wip

--- com.myorg.UI5Exercise/uimodule/webapp/controller/MainView.controller.js
+++ com.myorg.UI5Exercise/uimodule/webapp/controller/MainView.controller.js
@@ -1,23 +1,11 @@
-sap.ui.define(
-    ["./BaseController",'sap/m/MessageToast'],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller,MessageToast) {
-        "use strict";
-
-        var PageController = Controller.extend("sap.m.sample.Button.Page", {
-
-            onPress: function (evt) {
-                MessageToast.show("This application shows restaurants near wuerzburg office. It also includes an random restaurant picker.");
-            }
-        });
-    
-        return PageController;
-        
-    }
-);
-
-
-
-
+sap.ui.define(["./BaseController",'sap/m/MessageToast'],
+ function (Controller) {
+	"use strict";
+	return Controller.extend("com.myorg.UI5Exercise.controller.MainView", {
+        onPress: function () {
+            this.navTo("RestaurantList.view");
+           // MessageToast.show("This application shows restaurants near wuerzburg office. It also includes an random restaurant picker.");
+            
+        }
+	});
+});
